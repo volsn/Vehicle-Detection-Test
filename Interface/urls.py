0c27aab76349f2e60730813a0462a01e1591d683
root <root@369152-ca66253.tmweb.ru>
2020-10-22 19:15:48 +0300

    Tired

--- Interface/urls.py
+++ Interface/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static, serve
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from classifier.models import Camera
 
@@ -31,5 +32,6 @@ urlpatterns = [
     re_path(r'^media/(?P<path>.*)$', serve, \
             {'document_root': settings.MEDIA_ROOT,})
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += staticfiles_urlpatterns()
 
 disactivate_all_cameras()
